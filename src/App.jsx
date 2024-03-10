dfa4cfe6517bb89b4a296deb5a51ebccc87916e8
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-03-10 16:17:22 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
     return (
         <>
-            <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+            <input type="text" placeholder="Digite o usuário Github" onBlur={(e) => setNomeUsuario(e.target.value)} />
 
             {nomeUsuario.length > 4 && (
                 <>
